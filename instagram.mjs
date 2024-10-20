3e49c15fb76fbd5da04e6b53b518bd01b6bfdd17
CamilleSarrazin <1373080@cegepmontpetit.ca>
2024-10-20 17:33:07 -0400

    Add caching to Instagram API

--- instagram.mjs
+++ instagram.mjs
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import { instagramAccessToken } from './config.mjs';
 
+var cachedImages = null;
+var lastFetched = null;
+
 // Function to fetch and display Instagram feed
 async function getInstagramFeed() {
+    if (isCached(15)) {
+        console.log("using cache")
+        return cachedImages;
+    }
     var images = [];
     var nextUrl = `https://graph.instagram.com/v21.0/17841448618671800/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${instagramAccessToken}`;
     console.log("wow")
@@ -13,8 +20,26 @@ async function getInstagramFeed() {
         nextUrl = response.data.paging.next;
     }
     console.log("fini")
-    console.log(images)
-    return JSON.stringify(images);
+    cachedImages = JSON.stringify(images);
+    lastFetched = new Date();
+    return cachedImages;
+}
+
+function isCached(diffMins) {
+    if (cachedImages === null) {
+        return false;
+    }
+
+    if (lastFetched === null) {
+        return false;
+    }
+
+    const dateToRefresh = new Date(lastFetched.getTime() + diffMins*60000);
+    if (dateToRefresh < new Date()) {
+        return false;
+    }
+
+    return true;
 }
 
 export { getInstagramFeed }
