60ee53f75e7012fa146def09b80dbda0b028445d
CamilleSarrazin <1373080@cegepmontpetit.ca>
2024-10-20 17:19:44 -0400

    Add new CSS for header

--- server.mjs
+++ server.mjs
@@ -1,13 +1,23 @@
 import express from 'express';
+import path from 'path';
 import { getInstagramFeed } from './instagram.mjs';
 
 const app = express();
 const port = 3000;
-app.use(express.static('public'));
+const __dirname = path.resolve();
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
 
 app.get('/insta', async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(await getInstagramFeed());
+});
+
+app.get('/apropos', async (req, res) => {
+  const options = {
+      root: publicPath
+  };
+  res.sendFile('apropos.html', options);
 });
 
 app.listen(port, () => {
